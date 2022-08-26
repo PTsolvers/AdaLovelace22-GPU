836116aa50347740ee4c24358a1916f4022dbff8
Ivan Utkin <iutkin@ethz.ch>
2022-08-26 17:50:42 +0200

    Update formulation

--- free_surface_flow_2D.jl
+++ free_surface_flow_2D.jl
@@ -13,7 +13,7 @@ using Plots.PlotMeasures
     npow    = 1.0/3.0
     sinα    = sin(π/12)
     # dimensionally independent
-    ly,lz   = 2.0,1.0 # [m]
+    ly,lz   = 1.0,1.0 # [m]
     k0      = 1.0     # [Pa*s^npow]
     ρg      = 1.0     # [Pa/m]
     # scales
@@ -29,7 +29,7 @@ using Plots.PlotMeasures
     ηrel    = 1e-2
     maxiter = 200max(ny,nz)
     ncheck  = 5max(ny,nz)
-    re      = π/6
+    re      = π/7
     # preprocessing
     dy,dz   = ly/ny,lz/nz
     yv,zv   = LinRange(-ly/2,ly/2,ny+1),LinRange(0.0,lz,nz+1)
@@ -47,17 +47,18 @@ using Plots.PlotMeasures
         eII                   .= sqrt.((avz(diff(vx,dims=1)./dy)).^2 .+ (avy(diff(vx,dims=2)./dz)).^2)
         ηeff[2:end-1,2:end-1] .= ηeff[2:end-1,2:end-1].*(1.0-ηrel) .+ ηrel./(1.0./(k0.*av4(eII).^(npow-1.0)) .+ 1.0/ηreg)
         bc2!(ηeff)
-        τxy                  .+= (.-τxy .+ avy(ηeff).*diff(vx,dims=1)./dy)./(1.0 + 1.5cfl*ny/re)
-        τxz                  .+= (.-τxz .+ avz(ηeff).*diff(vx,dims=2)./dz)./(1.0 + 1.5cfl*ny/re)
+        τxy                  .+= (.-τxy .+ avy(ηeff).*diff(vx,dims=1)./dy)./(1.0 + 2cfl*ny/re)
+        τxz                  .+= (.-τxz .+ avz(ηeff).*diff(vx,dims=2)./dz)./(1.0 + 2cfl*ny/re)
         vx[2:end-1,2:end-1]  .+= (diff(τxy[:,2:end-1],dims=1)./dy .+ diff(τxz[2:end-1,:],dims=2)./dz .+ ρg*sinα).*(vdτ*lz/re)./ηeff[2:end-1,2:end-1]
         vx[:,end]             .= vx[:,end-1]
+        vx[1,:]               .= vx[2,:]
         if iter % ncheck == 0
             err = maximum(abs.(diff(τxy[:,2:end-1],dims=1)./dy .+ diff(τxz[2:end-1,:],dims=2)./dz .+ ρg*sinα))*lz/psc
             push!(iters_evo,iter/nz);push!(errs_evo,err)
             p1 = heatmap(yc,zc,vx'  ;aspect_ratio=1,xlabel="y",ylabel="z",title="Vx",xlims=(-ly/2,ly/2),ylims=(0,lz),right_margin=10mm)
             p2 = heatmap(yc,zc,ηeff';aspect_ratio=1,xlabel="y",ylabel="z",title="ηeff",xlims=(-ly/2,ly/2),ylims=(0,lz),colorbar_scale=:log10)
             p3 = plot(iters_evo,errs_evo;xlabel="niter/nx",ylabel="err",yscale=:log10,framestyle=:box,legend=false,markershape=:circle)
-            display(plot(p1,p2,p3;size=(1600,300),layout=(1,3),bottom_margin=10mm,left_margin=10mm))
+            display(plot(p1,p2,p3;size=(1200,400),layout=(1,3),bottom_margin=10mm,left_margin=10mm))
             @printf("  #iter/nz=%.1f,err=%1.3e\n",iter/nz,err)
         end
         iter += 1
