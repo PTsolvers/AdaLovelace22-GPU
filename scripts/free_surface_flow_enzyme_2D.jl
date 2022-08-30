04e4b078374a5f7eb415edacb650297ac6ecd365
Ivan Utkin <iutkin@ethz.ch>
2022-08-30 12:05:52 +0200

    WIP adjoint solver

--- scripts/free_surface_flow_enzyme_2D.jl
+++ scripts/free_surface_flow_enzyme_2D.jl
@@ -2,34 +2,42 @@ using Plots,Printf
 using Plots.PlotMeasures
 using Enzyme
 
-@views av(A)   = 0.5.*(A[1:end-1] .+ A[2:end])
-@views avy(A)  = 0.5.*(A[1:end-1,:] .+ A[2:end,:])
-@views avz(A)  = 0.5.*(A[:,1:end-1] .+ A[:,2:end])
-@views av4(A)  = 0.25.*(A[1:end-1,1:end-1] .+ A[1:end-1,2:end] .+ A[2:end,1:end-1] .+ A[2:end,2:end])
-@views bc2!(A) = (A[[1,end],:] .= A[[2,end-1],:]; A[:,[1,end]] .= A[:,[2,end-1]])
+@inline hmean(a,b) = 1.0/(1.0/a + 1.0/b)
 
-macro eII_xy()  esc(:( sqrt.((diff(vx[:,2:end-1],dims=1)./dy).^2 .+ (av4(diff(vx,dims=2)./dz)).^2)   )) end
-macro eII_xz()  esc(:( sqrt.((av4(diff(vx,dims=1)./dy)).^2 .+ (diff(vx[2:end-1,:],dims=2)./dz).^2)   )) end
-macro ηeff_xy() esc(:( 1.0./(1.0./(k0.*@eII_xy().^(npow-1.0)) .+ 1.0/ηreg)                           )) end
-macro ηeff_xz() esc(:( 1.0./(1.0./(k0.*@eII_xz().^(npow-1.0)) .+ 1.0/ηreg)                           )) end
+macro ∂vx_∂y(iy,iz) esc(:( (vx[$iy+1,$iz] - vx[$iy,$iz])/dy )) end
+macro ∂vx_∂z(iy,iz) esc(:( (vx[$iy,$iz+1] - vx[$iy,$iz])/dz )) end
 
-macro eII()     esc(:( sqrt.((avz(diff(vx,dims=1)./dy)).^2 .+ (avy(diff(vx,dims=2)./dz)).^2)         )) end
-macro ηeff()    esc(:( 1.0./(1.0./(k0.*av4(@eII()).^(npow-1.0)) .+ 1.0/ηreg)                         )) end
+macro ∂vx_∂y_a4(iy,iz) esc(:( 0.25*(@∂vx_∂y($iy,$iz) + @∂vx_∂y($iy+1,$iz) + @∂vx_∂y($iy,$iz+1) + @∂vx_∂y($iy+1,$iz+1)) )) end
+macro ∂vx_∂z_a4(iy,iz) esc(:( 0.25*(@∂vx_∂z($iy,$iz) + @∂vx_∂z($iy+1,$iz) + @∂vx_∂z($iy,$iz+1) + @∂vx_∂z($iy+1,$iz+1)) )) end
 
-macro ηeffτ()   esc(:( max.(ηeff_xy[1:end-1,:],ηeff_xy[2:end,:],ηeff_xz[:,1:end-1],ηeff_xz[:,2:end]) )) end
-macro τxy()     esc(:( @ηeff_xy().*diff(vx[:,2:end-1],dims=1)./dy )) end
-macro τxz()     esc(:( @ηeff_xz().*diff(vx[2:end-1,:],dims=2)./dz )) end
+macro τxy(iy,iz) esc(:( @ηeff_xy($iy,$iz)*@∂vx_∂y($iy,$iz+1) )) end
+macro τxz(iy,iz) esc(:( @ηeff_xz($iy,$iz)*@∂vx_∂z($iy+1,$iz) )) end
 
-@views function residual!(r_vx,vx,k0,npow,ηreg,ρg,sinα,dy,dz)
-    r_vx .= diff(@τxy(),dims=1)./dy .+ diff(@τxz(),dims=2)./dz .+ ρg*sinα
+macro eII_xy(iy,iz) esc(:( sqrt(@∂vx_∂y($iy,$iz+1)^2 + @∂vx_∂z_a4($iy,$iz)^2) )) end
+macro eII_xz(iy,iz) esc(:( sqrt(@∂vx_∂y_a4($iy,$iz)^2 + @∂vx_∂z($iy+1,$iz)^2) )) end
+
+macro ηeff_xy(iy,iz) esc(:( hmean(k0*@eII_xy($iy,$iz)^(npow-1.0), ηreg) )) end
+macro ηeff_xz(iy,iz) esc(:( hmean(k0*@eII_xz($iy,$iz)^(npow-1.0), ηreg) )) end
+
+macro ηeffτ(iy,iz) esc(:( max(ηeff_xy[$iy,$iz],ηeff_xy[$iy+1,$iz],ηeff_xz[$iy,$iz],ηeff_xz[$iy,$iz+1]) )) end
+
+function residual!(r_vx,vx,k0,npow,ηreg,ρg,sinα,dy,dz)
+    for iz = axes(r_vx,2), iy = axes(r_vx,1)
+        r_vx[iy,iz] = (@τxy(iy+1,iz)-@τxy(iy,iz))/dy + (@τxz(iy,iz+1)-@τxz(iy,iz))/dz + ρg*sinα
+    end
+    return
+end
+
+function ∂r_∂v!(JVP,vect,r_vx,vx,k0,npow,ηreg,ρg,sinα,dy,dz)
+    Enzyme.autodiff(residual!,Duplicated(r_vx,vect),Duplicated(vx,JVP),Const(k0),Const(npow),Const(ηreg),Const(ρg),Const(sinα),Const(dy),Const(dz))
     return
 end
 
 @views function main()
     # physics
     # non-dimensional
-    npow    = 1.0/3.0
-    sinα    = sin(π/12)
+    npow    = 1.0/1.0
+    sinα    = sin(π/6)
     # dimensionally independent
     ly,lz   = 1.0,1.0 # [m]
     k0      = 1.0     # [Pa*s^npow]
@@ -59,23 +67,35 @@ end
     ηeff_xz = zeros(ny-2,nz-1)
     τxy     = zeros(ny-1,nz-2)
     τxz     = zeros(ny-2,nz-1)
+    JVP     = zeros(ny-2,ny-2)
+    vect    = zeros(ny-2,ny-2)
+    ∂r_∂v!(JVP,vect,r_vx,vx,k0,npow,ηreg,ρg,sinα,dy,dz)
     # action
     iters_evo = Float64[]; errs_evo = Float64[]; err = 2ϵtol; iter = 1
     while err >= ϵtol && iter <= maxiter
-        ηeff_xy              .= ηeff_xy.*(1.0-ηrel) .+ ηrel.*@ηeff_xy()
-        ηeff_xz              .= ηeff_xz.*(1.0-ηrel) .+ ηrel.*@ηeff_xz()
-        τxy                 .+= (.-τxy .+ ηeff_xy.*diff(vx[:,2:end-1],dims=1)./dy)./(1.0 + 2cfl*ny/re)
-        τxz                 .+= (.-τxz .+ ηeff_xz.*diff(vx[2:end-1,:],dims=2)./dz)./(1.0 + 2cfl*ny/re)
-        vx[2:end-1,2:end-1] .+= (diff(τxy,dims=1)./dy .+ diff(τxz,dims=2)./dz .+ ρg*sinα).*(vdτ*lz/re)./@ηeffτ()
+        for iz = union(axes(τxy,2),axes(τxz,2)), iy = union(axes(τxy,1),axes(τxz,1))
+            if iy ∈ axes(τxy,1) && iz ∈ axes(τxy,2)
+                ηeff_xy[iy,iz] = ηeff_xy[iy,iz]*(1.0-ηrel) + ηrel*@ηeff_xy(iy,iz)
+                τxy[iy,iz]    += (-τxy[iy,iz] + ηeff_xy[iy,iz]*@∂vx_∂y(iy,iz+1))/(1.0 + 2cfl*ny/re)
+            end
+            if iy ∈ axes(τxz,1) && iz ∈ axes(τxz,2)
+                ηeff_xz[iy,iz] = ηeff_xz[iy,iz]*(1.0-ηrel) + ηrel*@ηeff_xz(iy,iz)
+                τxz[iy,iz]    += (-τxz[iy,iz] + ηeff_xz[iy,iz]*@∂vx_∂z(iy+1,iz))/(1.0 + 2cfl*ny/re)
+            end
+        end
+        for iz = axes(r_vx,2), iy = axes(r_vx,1)
+            vx[iy+1,iz+1] += ((τxy[iy+1,iz]-τxy[iy,iz])/dy + (τxz[iy,iz+1]-τxz[iy,iz])/dz + ρg*sinα)*(vdτ*lz/re)/@ηeffτ(iy,iz)
+        end
         vx[:,end] .= vx[:,end-1]; vx[1,:] .= vx[2,:]
         if iter % ncheck == 0
             residual!(r_vx,vx,k0,npow,ηreg,ρg,sinα,dy,dz)
             err = maximum(abs.(r_vx))*lz/psc
             push!(iters_evo,iter/nz);push!(errs_evo,err)
-            p1 = heatmap(yc,zc,vx'  ;aspect_ratio=1,xlabel="y",ylabel="z",title="Vx",xlims=(-ly/2,ly/2),ylims=(0,lz),right_margin=10mm)
-            p2 = heatmap(yc[2:end-1],zc[2:end-1],(@ηeff())';aspect_ratio=1,xlabel="y",ylabel="z",title="ηeff",xlims=(-ly/2,ly/2),ylims=(0,lz),colorbar_scale=:log10)
+            p1 = heatmap(yc,zc,vx';aspect_ratio=1,xlabel="y",ylabel="z",title="Vx",xlims=(-ly/2,ly/2),ylims=(0,lz),right_margin=10mm)
+            p2 = heatmap(yc[2:end-1],zc[2:end-1],r_vx';aspect_ratio=1,xlabel="y",ylabel="z",title="ηeff",xlims=(-ly/2,ly/2),ylims=(0,lz))
             p3 = plot(iters_evo,errs_evo;xlabel="niter/nx",ylabel="err",yscale=:log10,framestyle=:box,legend=false,markershape=:circle)
-            display(plot(p1,p2,p3;size=(1200,400),layout=(1,3),bottom_margin=10mm,left_margin=10mm))
+            p4 = plot(yc,vx[:,end];xlabel="y",ylabel="Vx",framestyle=:box,legend=false)
+            display(plot(p1,p2,p3,p4;size=(800,800),layout=(2,2),bottom_margin=10mm,left_margin=10mm,right_margin=10mm))
             @printf("  #iter/nz=%.1f,err=%1.3e\n",iter/nz,err)
         end
         iter += 1
